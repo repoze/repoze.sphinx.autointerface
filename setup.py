bcd39314a364e6e0871a7e8ea07e471249e9b06e
Steve Piercy <web@stevepiercy.com>
2021-05-16 01:04:35 -0700

    Blackify

--- setup.py
+++ setup.py
@@ -20,19 +20,19 @@ def readfile(name):
         return f.read()
 
 
-README = readfile('README.rst')
-CHANGES = readfile('CHANGES.rst')
+README = readfile("README.rst")
+CHANGES = readfile("CHANGES.rst")
 
 tests_require = [
-    'zope.testrunner',
+    "zope.testrunner",
 ]
 
-setup(name='repoze.sphinx.autointerface',
-      version='0.8.1.dev0',
-      description='Sphinx extension: auto-generates API docs '
-                  'from Zope interfaces',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name="repoze.sphinx.autointerface",
+    version="0.8.1.dev0",
+    description="Sphinx extension: auto-generates API docs " "from Zope interfaces",
+    long_description=README + "\n\n" + CHANGES,
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
@@ -42,29 +42,29 @@ setup(name='repoze.sphinx.autointerface',
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        ],
-      keywords='web wsgi zope Sphinx',
-      author="Agendaless Consulting",
-      author_email="repoze-dev@lists.repoze.org",
-      url="https://github.com/repoze/repoze.sphinx.autointerface/",
-      project_urls={
-          "Documentation": "https://github.com/repoze/repoze.sphinx.autointerface/",
-          "Changelog": "https://github.com/repoze/repoze.sphinx.autointerface/blob/master/CHANGES.rst",
-          "Issue Tracker": "https://github.com/repoze/repoze.sphinx.autointerface/issues",
-      },
-      license="BSD-derived (Repoze)",
-      packages=find_packages(),
-      include_package_data=True,
-      namespace_packages=['repoze', 'repoze.sphinx'],
-      zip_safe=False,
-      python_requires=">=3.6",
-      tests_require = tests_require,
-      install_requires=[
-          'zope.interface',
-          'Sphinx>=1.0',
-          'setuptools',
-      ],
-      extras_require = {
-          'test': tests_require,
-      },
-      )
+    ],
+    keywords="web wsgi zope Sphinx",
+    author="Agendaless Consulting",
+    author_email="repoze-dev@lists.repoze.org",
+    url="https://github.com/repoze/repoze.sphinx.autointerface/",
+    project_urls={
+        "Documentation": "https://github.com/repoze/repoze.sphinx.autointerface/",
+        "Changelog": "https://github.com/repoze/repoze.sphinx.autointerface/blob/master/CHANGES.rst",
+        "Issue Tracker": "https://github.com/repoze/repoze.sphinx.autointerface/issues",
+    },
+    license="BSD-derived (Repoze)",
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=["repoze", "repoze.sphinx"],
+    zip_safe=False,
+    python_requires=">=3.6",
+    tests_require=tests_require,
+    install_requires=[
+        "zope.interface",
+        "Sphinx>=1.0",
+        "setuptools",
+    ],
+    extras_require={
+        "test": tests_require,
+    },
+)
